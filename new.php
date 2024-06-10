8b3c4869cf5d4d2eca196bcc7ba22044e73f1575
christianbernal7 <christiandave.bernal63@gmail.com>
2024-06-10 15:53:17 +0800

    postposttt

--- new.php
+++ new.php
@@ -2,62 +2,78 @@
 
 <?php
 
-if($_SERVER['REQUEST_METHOD'] == "POST"){
-    $ch = curl_init();
 
-    // Set the target URL
-    curl_setopt($ch, CURLOPT_URL, "http://localhost/php_curl_post/post_api.php");
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($_POST));
+if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['post_id'])) {
+    $postId = intval($_POST['post_id']);
+    $ch = curl_init();
+    $url = "https://jsonplaceholder.typicode.com/posts/$postId";
+
+    curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-
     $output = curl_exec($ch);
-
- 
     $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
     curl_close($ch);
-
     
+    if ($http_code == 200) {
+        $post_data = json_decode($output, true);
+    }
 }
 ?>
 
-<br>
-
-<div class="container">
+<div class="container mt-4">
     <div class="row justify-content-center">
         <div class="col-md-6">
-            <form action="" method="POST" id="sample-form"  style="border: 2px solid; border-radius: 10px; padding: 8px;">
+            <form action="" method="POST" id="search-form" style="border: 2px solid; border-radius: 10px; padding: 8px;">
                 <div class="form-group">
-                    <label for="name">Name</label>
-                    <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+                    <label for="post_id">Search ID</label>
+                    <input type="text" class="form-control" id="post_id" name="post_id" placeholder="Enter Post ID">
                 </div>
-                <div class="form-group">
-                    <label for="contact_number">Contact Number</label>
-                    <input type="text" class="form-control" id="contact_number" name="contact_number" placeholder="Contact Number">
+                <div class="buttons d-flex justify-content-center">
+                    <button type="submit" class="btn btn-primary" style="background-color: blue; border: 1px solid black;">Search</button>
                 </div>
-                <div class="form-group">
-                    <label for="address">Address</label>
-                    <input type="text" class="form-control" id="address" name="address" placeholder="Address">
-            </div>
-            <div class="buttons d-flex justify-content-center">
-                <button type="submit" name="action" value="submit" class="btn btn-primary mr-2" style="background-color: green; border: 1px solid black;">Submit</button>
-                <button type="submit" name="action" value="update" class="btn btn-primary mr-2" style="background-color: yellow; border: 1px solid black;">Update</button>
-                <button type="submit" name="action" value="delete" class="btn btn-primary mr-2" style="background-color: red; border: 1px solid black;">Delete</button>
-            </div>
             </form>
         </div>
     </div>
+
+    <?php if (isset($post_data) && !empty($post_data)): ?>
+    <div class="row justify-content-center mt-4">
+        <div class="col-md-8">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>User ID</th>
+                        <th>Title</th>
+                        <th>Body</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><?php echo htmlspecialchars($post_data['id']); ?></td>
+                        <td><?php echo htmlspecialchars($post_data['userId']); ?></td>
+                        <td><?php echo htmlspecialchars($post_data['title']); ?></td>
+                        <td><?php echo htmlspecialchars($post_data['body']); ?></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <?php elseif (isset($post_data)): ?>
+    <div class="row justify-content-center mt-4">
+        <div class="col-md-6">
+            <div class="alert alert-danger" role="alert">
+                Post not found.
+            </div>
+        </div>
+    </div>
+    <?php endif; ?>
 </div>
 
-<?php 
 
-    echo "<pre>";
-    print_r($_POST);
-    echo "</pre>";
-        
-?>
+
 
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
